1ec8ac1686a239bc2bd1656e554f5e1d1afe1c4c
ZachF53 <37436051+ZachF53@users.noreply.github.com>
2018-05-22 18:09:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Contact Route
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
