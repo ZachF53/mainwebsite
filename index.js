97e2351f9f573d72e5ce69cc9014e9f6e56b59c2
Unknown <football45353@gmail.com>
2018-05-31 19:40:25 -0500

    index

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.set('port', (process.env.PORT || 80))
 // Load View
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,22 +17,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Contact Route
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   res.render('index');
 });
 
 // Add Route
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/port', (req, res) => {
   res.render('port');
 });
 app.get('/service', (req, res) => {
-  res.render('service');
+  res.render('service1');
 });
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+app.get('/blog', (req, res) => {
+  res.render('blog');
+});
+app.get('/landing', (req, res) => {
+  res.render('landing');
+});
+app.get('/paralax', (req, res) => {
+  res.render('paralax');
 });
 
 app.post('/send', (req, res) => {
@@ -41,8 +49,10 @@ app.post('/send', (req, res) => {
     <p>You have a new contact request</p>
     <h3>Contact Details</h3>
     <ul>
-      <li>Name: ${req.body.firstname} ${req.body.lastname}</li>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
       <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
     </ul>
     <h3>Message</h3>
     <p>${req.body.message}</p>
@@ -50,12 +60,12 @@ app.post('/send', (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'imap-mail.outlook.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'frwebdesigns1@gmail.com', // generated ethereal user
-        pass: 'Password@53'  // generated ethereal password
+        user: 'zachery@firstresponsewebdesign.com', // generated ethereal user
+        pass: '56nTk46!'  // generated ethereal password
     },
     tls:{
       rejectUnauthorized:false
@@ -64,9 +74,9 @@ app.post('/send', (req, res) => {
 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Website Contact" <frwebdesigns1@gmail.com>', // sender address
-      to: 'football45353@gmail.com, frwebdesigns1@gmail.com', // list of receivers
-      subject: 'Website Contact Request', // Subject line
+      from: '"Nodemailer Contact" <zachery@firstresponsewebdesign.com>', // sender address
+      to: 'football45353@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
       text: 'Contact Request', // plain text body
       html: output // html body
   };
@@ -84,6 +94,4 @@ app.post('/send', (req, res) => {
 });
 
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(3000, () => console.log('Server started...'));
